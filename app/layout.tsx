b9d535559ce4e2e7817c3876bdebfaf12b7e1162
mykhatilo-vlad <mykhatilo.vlad@gmail.com>
2025-06-22 13:08:23 +0300

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {meta} from './data/content.json';
+import { meta } from './data/content.json';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
